c0ba3a885830cd060ff9d8bdfe4d45006882b013
franco caspani <francocasp@gmail.com>
2022-06-06 19:32:55 -0300

    nav-bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
 
 import '../stylesheets/App.css';
+import Header2 from './Header';
+import bgvideo from '../video/bgvideo.mp4';
+import Carousel from './Carousel';
+import data from '../data';
 
 function App() {
   return (
     <div className="App">
+      <div className='main-container'>
+        <video src={bgvideo} autoPlay loop muted className='video-bg'>
+        </video>
+        <Header2 />
+        <div className='hero-container'>
+          <h1>My Tinerary</h1>
+          <p>Find your perfect trip, designed by insiders who know and love their cities!</p>
+        </div>
+      
+      </div>
+        <div className='carousel-container'>
+        <Carousel data = {data}/>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,66 @@
+
+
+const data = [
+    {   
+        id : 1,
+        city : 'Melbourne',
+        img : '../img/melbourne.jpeg'
+    },
+    {   
+        id : 2,
+        city : 'Venice',
+        img : '../img/venice.jpeg'
+    },
+    {   
+        id : 3,
+        city : 'Buenos Aires',
+        img : '../img/bsas.jpg'
+    },
+    {   
+        id : 4,
+        city : 'Aarhus',
+        img : '../img/aarhus.jpeg'
+    },
+    {   
+        id : 5,
+        city : 'Cophenaguen',
+        img : '../img/cophenaguen.jpeg'
+    },
+    {   
+        id : 6,
+        city : 'Sydney',
+        img : '../img/sydney.jpeg'
+    },
+    {   
+        id : 7,
+        city : 'Roma',
+        img : '../img/roma.jpeg'
+    },
+    {   
+        id : 8,
+        city : 'San Francisco',
+        img : '../img/san-francisco.jpeg'
+    },
+    {   
+        id : 9,
+        city : 'Brisbane',
+        img : '../img/brisbane.jpeg'
+    },
+    {   
+        id : 10,
+        city : 'Berlin',
+        img : '../img/berlin.jpeg'
+    },
+    {   
+        id : 11,
+        city : 'Stockholm',
+        img : '../img/stockholm.jpeg'
+    },
+    {   
+        id : 12,
+        city : 'Cordoba',
+        img : '../img/cordoba.jpeg'
+    },
+]
+
+export default data;

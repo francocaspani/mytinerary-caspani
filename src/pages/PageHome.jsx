c64c89bb5d7ea0eedf1cc010297941a7637be642
franco caspani <francocasp@gmail.com>
2022-06-08 14:28:17 -0300

    scroll to top

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,14 +1,23 @@
 import data from '../data';
 import Hero from '../components/Hero';
 import Carousel from '../components/Carousel';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
+const useScrollToTop = () => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, [location]);
+};
 
 
 export default function PageHome() {
-    return (
-        <>
-        <Hero />
-        <Carousel data = {data}/>
-        </>
-    )
+  useScrollToTop()
+  return (
+    <>
+      <Hero />
+      <Carousel data={data} />
+    </>
+  )
 }

9b13bc0de90b14ac95d4ae7041ce349f218865cf
franco caspani <francocasp@gmail.com>
2022-07-08 16:21:59 -0300

    url update

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -21,7 +21,7 @@ import {AnimatePresence} from 'framer-motion'
 import PageLogIn from '../pages/PageLogIn';
 import PageAccount from '../pages/PageAccount';
 
-export const urlBackend = 'https://mytinerary-caspani-back.herokuapp.com'
+export const urlBackend = 'https://mytinerary-caspani-back.herokuapp.com/api'
 
 const Toast = Swal.mixin({
   toast: true,
